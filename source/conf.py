62853b4dfb915e3466f3e3b8389a5b0ce33a07cf
Josh Siegle <joshs@alleninstitute.org>
2023-07-16 13:32:03 -0700

    Fix GitHub links

--- source/conf.py
+++ source/conf.py
@@ -119,9 +119,10 @@ html_theme_options = {
     "navbar_end": ["navbar-icon-links"],
     "navbar_align": "content",
     "footer_start": ["copyright"],
+    "external_links": [{"name": "Open Ephys", "url": "https://open-ephys.org"},],
     'icon_links': [
         dict(name='GitHub',
-             url='https://github.com/open-ephys/commutator-docs',
+             url='https://github.com/open-ephys/onix-fmc-host',
              icon='fab fa-github'),
         dict(name='Twitter',
              url='https://twitter.com/openephys',
